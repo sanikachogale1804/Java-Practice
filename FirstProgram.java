1de05dbc6f7ac9fa3551b3035d4f9ec23cd97df2
unknown <chogalesanika8@gmail.com>
2024-08-28 14:24:27 +0530

    initial

---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+class FirstProgram
+{
+    public static void main(String args[])
+	{
+		byte sanika=100;
+		System.out.println(sanika+" Stored in a variable");
+		
+		short b=32223;
+		System.out.println("value of b is:"+b);
+		
+		int a=99487838;
+		System.out.println("value of a is:"+a);
+		
+		float c=12.96F;
+		System.out.println(c+" stored in a variable");
+	}
+	
+}
