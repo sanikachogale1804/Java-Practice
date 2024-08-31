0f745eefdebbc95ed1dcfcbfddc3444dfc0b27df
unknown <chogalesanika8@gmail.com>
2024-08-31 19:27:27 +0530

    initial

---
+++ TernaryOperaor.java
@@ -0,0 +1,7 @@
+class TernaryOperator
+{ 
+   public static void main (String args[])
+   {
+	   System.out.println(true?"sneha":"akshata");
+   }
+}
---
+++ TernaryOperator.java
@@ -0,0 +1,16 @@
+class TernaryOperator
+{ 
+   public static void main (String args[])
+   {
+	   System.out.println(true?"sneha":"akshata");
+	   
+	   int a=60;
+	   int b=40;
+	   int biggervalue=a>b?a:b;
+	   System.out.println(biggervalue);
+	   System.out.println(a>b?"a is greater":"b is greater");
+	   
+	   int age=20;
+	   System.out.println(age>38?"true":"false");
+   }
+}
