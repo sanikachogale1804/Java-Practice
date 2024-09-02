c2ce0f64226fde8c0bf1a2e65f8830d7384e1cb6
unknown <chogalesanika8@gmail.com>
2024-09-02 14:32:02 +0530

    initial

---
+++ p3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class p3
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		System.out.println("value given by user:"+a);
+		
+		System.out.println("Enter String");
+		String name=sc.next();
+		System.out.println("My name is :"+name);
+		
+		String sentence=sc.nextLine();
+		System.out.println("sentence is:"+sentence);
+		
+		int age=sc.nextInt();
+		System.out.println("age is:"+age);
+		
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		System.out.println(c-d);
+	}
+}
