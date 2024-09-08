6846bbf94c1153d527cce1b33c514e4f3c05bef1
unknown <chogalesanika8@gmail.com>
2024-09-08 10:25:40 +0530

    initial

---
+++ p12.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class p12
+{
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		System.out.println("1:Add 2:Sub 3:Mul 4:Div");
+		int decision=sc.nextInt();
+		
+		if(decision==1)
+		{
+			System.out.println(n1+n2);
+		}
+		else if(decision==2)
+		{
+			System.out.println(n1-n2);
+		}
+		else if(decision==3)
+		{
+			System.out.println(n1*n2);
+		}
+		else if(decision==4)
+		{
+			System.out.println(n1/n2);
+		}
+		
+	}
+}
