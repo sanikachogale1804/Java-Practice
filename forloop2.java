489c4aedb953f0b0fd9bf9d2ce8260ef8dca8a58
unknown <chogalesanika8@gmail.com>
2024-09-14 19:20:17 +0530

    initial

---
+++ forloop2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class forloop2
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n*i);
+		}
+	}
+}
