11cf631d8fcb23fa223097641191db359c250237
unknown <chogalesanika8@gmail.com>
2024-09-14 18:55:06 +0530

    initial

---
+++ while4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class while4
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=0;
+		
+		while(n<=0)
+		{
+			n=sc.nextInt();
+		}
+	}
+}
