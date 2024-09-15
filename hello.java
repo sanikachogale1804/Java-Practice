d9a33994558a6719b706ca51511766eeb9653ef6
unknown <chogalesanika8@gmail.com>
2024-09-15 12:59:41 +0530

    initial

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hello
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String h=sc.next();
+	    System.out.println(h);	
+	}
+}
