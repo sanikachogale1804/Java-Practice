d6cb406b2a9dbaad25b4d9fdc81205db0a6152cd
unknown <chogalesanika8@gmail.com>
2024-09-15 12:44:45 +0530

    initial

--- alphabets.java
+++ alphabets.java
@@ -4,9 +4,10 @@ class alphabets
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		char s=sc.next().charAt(0);
+		char string=sc.next().charAt(0);
+		char s;
 		System.out.println("Enter string:");
-		for(s='a';s<='z';s++)
+		for(s=string;s<='z';s++)
 		{
 			System.out.println(s);
 		}
