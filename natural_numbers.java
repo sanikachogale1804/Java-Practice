c8ffb6c71265812222e9a8c777641d9131802948
unknown <chogalesanika8@gmail.com>
2024-09-15 12:25:12 +0530

    initial

---
+++ natural_numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class natural_numbers
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter starting number");
+		int s=sc.nextInt();
+		System.out.println("Enter ending number");
+		int e=sc.nextInt();
+		
+		while(s<=e)
+		{
+			System.out.println(s);
+			s++;
+		}
+	}
+}
