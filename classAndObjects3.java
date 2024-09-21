90f46833fdc7a0fc3362f0e875fda288526a7bbc
unknown <chogalesanika8@gmail.com>
2024-09-21 22:47:03 +0530

    initial

---
+++ classAndObjects3.java
@@ -0,0 +1,28 @@
+class product
+{
+	int id;
+	String name;
+}
+
+
+
+class classAndObjects3
+{
+	public static void main(String args[])
+	{
+		product p1=new product();
+		product p2=new product();
+		p1.id=1;
+		p1.name="mobile";
+		
+		System.out.println(p1.id);
+		System.out.println(p1.name);
+		
+		p2.id=2;
+		p2.name="laptop";
+		
+		System.out.println(p2.id);
+		System.out.println(p2.name);
+		
+	}
+}
