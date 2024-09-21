187d4a14de9cf4b7c915f7d9b828e69c961c3ceb
unknown <chogalesanika8@gmail.com>
2024-09-21 22:49:52 +0530

    initial

---
+++ p18.java
@@ -0,0 +1,18 @@
+class School
+{
+	int schoolId;
+	String schoolName;
+	
+	void setSchoolDetails(int schoolId,String schoolName);
+}
+
+class p18
+{
+	public static void main(String args[])
+	{
+		
+	}
+}
+//collection of classe matlab package
+//package ka collection matlab jar
+//javaSE(Standard Environment)
